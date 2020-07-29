5f08a7d03de62bd3ea21c2f3ada824d66361749e
cavis <ryancavis@gmail.com>
2020-07-29 10:55:58 -0600

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PrxAuth.Mixfile do
 
   def project do
     [app: :prx_auth,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
